0f6499398a5b3f0973f0038867a5b26df81036b8
aadarshps3 <aadarshps3@gmail.com>
2023-07-06 12:38:06 +0530

    push

---
+++ todoapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from todoapp.models import todoapp
+
+
+class dateinput(forms.DateInput):
+    input_type = "date"
+
+class TodoForm(forms.ModelForm):
+    date=forms.DateField(widget=dateinput)
+    class Meta:
+        model=todoapp
+        fields='__all__'
